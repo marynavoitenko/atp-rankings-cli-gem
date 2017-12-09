6009c0abc3d3b99a3abd2ce447f40089beda859e
github username <github email address>
2017-12-09 18:55:31 -0500

    add file dependencies; add welcome message to cli call method

--- lib/atp_rankings_cli_gem.rb
+++ lib/atp_rankings_cli_gem.rb
@@ -1,4 +1,5 @@
-require "atp_rankings_cli_gem/version"
+require_relative "./atp_rankings_cli_gem/version"
+require_relative "./atp_rankings_cli_gem/cli"
 
 module AtpRankingsCliGem
   # Your code goes here...
--- lib/atp_rankings_cli_gem/cli.rb
+++ lib/atp_rankings_cli_gem/cli.rb
@@ -1,6 +1,8 @@
 class AtpRankingsCliGem::CLI
 
   def call
+    puts ""
+    puts "~~~ Welcome to ATP World Tour Rankings! ~~~"
   end
 
 end
