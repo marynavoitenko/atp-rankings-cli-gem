86a9ff80183c23ad37b361b00847890b62071162
github username <github email address>
2017-12-09 18:59:12 -0500

    display Top 10 rankings - hardcoded for now

--- lib/atp_rankings_cli_gem/cli.rb
+++ lib/atp_rankings_cli_gem/cli.rb
@@ -3,6 +3,13 @@ class AtpRankingsCliGem::CLI
   def call
     puts ""
     puts "~~~ Welcome to ATP World Tour Rankings! ~~~"
+    puts ""
+    puts "Top 10 Tennis Players:"
+    puts ""
+    puts "1. Rafael Nadal"
+    puts "2. Roger Federer"
+    puts "3. Marat Safin"
+    puts ""
   end
 
 end
