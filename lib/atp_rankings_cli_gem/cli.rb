40aaa4c008e2cef253b6e544e9ceacadbfd6efad
github username <github email address>
2017-12-09 19:20:59 -0500

    create user interactions in cli class

--- lib/atp_rankings_cli_gem/cli.rb
+++ lib/atp_rankings_cli_gem/cli.rb
@@ -10,6 +10,32 @@ class AtpRankingsCliGem::CLI
     puts "2. Roger Federer"
     puts "3. Marat Safin"
     puts ""
+    input = ""
+    until input == "exit" do
+      puts ""
+      puts "Type player's ranking (1-10) to learn about his recent activity."
+      puts "Type 'rankings' to see Top 10 players, or type 'exit'"
+      puts ""
+      input = gets.strip
+      if input == "rankings"
+        puts ""
+        puts "Top 10 Tennis Players:"
+        puts ""
+        puts "1. Rafael Nadal"
+        puts "2. Roger Federer"
+        puts "3. Marat Safin"
+        puts ""
+      elsif input.to_i.between?(1,10)
+        puts ""
+        puts "Last event played..."
+        puts "Last opponent played..."
+        puts ""
+      elsif input != "exit"
+        puts "O_o invalid input."
+      end
+
+    end
+    puts "See you later!"
   end
 
 end
