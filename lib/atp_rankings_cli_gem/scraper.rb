d266b134de679f9f7738526eefa1ec0948cb5427
github username <github email address>
2017-12-12 01:56:35 -0500

    remove unnesessary comments

--- lib/atp_rankings_cli_gem/scraper.rb
+++ lib/atp_rankings_cli_gem/scraper.rb
@@ -17,16 +17,6 @@ class AtpRankingsCliGem::Scraper
       AtpRankingsCliGem::Player.create_from_scraper(player)
     end
 
-    # name = page.css('td.player-cell').first.text.strip
-    # contry = page.css('td.country-cell div div img')[0]['alt']
-    # age = page.css('td.age-cell').first.text.strip
-    # url = page.css('td.player-cell a')[0]['href']
-
-    # from profile page - activity tab
-    # http://www.atpworldtour.com/en/players/rafael-nadal/n409/player-activity
-    # profile_page = Nokogiri::HTML(open('http://www.atpworldtour.com/en/players/rafael-nadal/n409/player-activity'))
-    # Last event played = profile_page.css('td.title-content a').first.text
-    # Last opponent played = profile_page.css('.day-table-name a').first.text
   end
 
   def self.scrape_profile_page(index)
